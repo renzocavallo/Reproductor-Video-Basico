6b218271a7febf16f1784aaf4846a67f08a3b16e
Renzo Cavallo <renzojcavallo@gmail.com>
2022-03-06 19:46:34 -0300

    Se agrega barra de sonido

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var reproductor = document.getElementById("reproductor")
 var botonPlay = document.getElementById("btonPlay")
 var botonReload = document.getElementById("btonReload")
 var botonBigSmall = document.getElementById("btonBigSmall")
+var botonSound = document.getElementById("silenciar")
+var volumen1 = document.getElementById("Sound1")
+var volumen2 = document.getElementById("Sound2")
+var volumen3 = document.getElementById("Sound3")
+var volumen4 = document.getElementById("Sound4")
+var volumen5 = document.getElementById("Sound5")
 
 botonPlay.addEventListener("click",function(){
     if(botonPlay.innerHTML != "Pause"){
@@ -18,6 +24,119 @@ botonReload.addEventListener("click",function(){
     botonPlay.innerHTML = "Pause"
     video.load()
     video.play()
+})
+
+botonSound.addEventListener("click",function(){
+    if(botonSound.innerHTML != "NotSound"){
+        botonSound.innerHTML = "NotSound"
+        video.muted = true
+        volumen1.style.backgroundColor = "grey"
+        volumen2.style.backgroundColor = "grey"
+        volumen3.style.backgroundColor = "grey"
+        volumen4.style.backgroundColor = "grey"
+        volumen5.style.backgroundColor = "grey"
+    }else{
+        botonSound.innerHTML = "Sound"
+        video.muted = false
+    }
+})
+
+volumen1.addEventListener("click",function(){
+    if(volumen1.style.backgroundColor != "black"){
+    volumen1.style.backgroundColor = "black"
+    video.volume = 0.1
+    if(volumen2.style.backgroundColor == "black" && 
+       volumen3.style.backgroundColor == "black" && 
+       volumen4.style.backgroundColor == "black" && 
+       volumen5.style.backgroundColor == "black"){
+       volumen2.style.backgroundColor = "grey"
+       volumen3.style.backgroundColor = "grey"
+       volumen4.style.backgroundColor = "grey"
+       volumen5.style.backgroundColor = "grey"
+       }
+    }else{
+    volumen1.style.backgroundColor = "black"
+    volumen2.style.backgroundColor = "grey"
+    volumen3.style.backgroundColor = "grey"
+    volumen4.style.backgroundColor = "grey"
+    volumen5.style.backgroundColor = "grey"
+    }
+})
+
+volumen2.addEventListener("click",function(){
+    if(volumen2.style.backgroundColor != "black"){
+        volumen2.style.backgroundColor = "black"
+        volumen1.style.backgroundColor = "black"
+        video.volume = 0.3
+        if(volumen3.style.backgroundColor == "black" && 
+        volumen4.style.backgroundColor == "black" && 
+        volumen5.style.backgroundColor == "black"){
+        volumen3.style.backgroundColor = "grey"
+        volumen4.style.backgroundColor = "grey"
+        volumen5.style.backgroundColor = "grey"
+        }
+    }else{
+    volumen2.style.backgroundColor = "black"
+    volumen1.style.backgroundColor = "black"
+    volumen3.style.backgroundColor = "grey"
+    volumen4.style.backgroundColor = "grey"
+    volumen5.style.backgroundColor = "grey"
+    }
+})
+
+volumen3.addEventListener("click",function(){
+    if(volumen3.style.backgroundColor != "black"){
+        volumen3.style.backgroundColor = "black"
+        volumen2.style.backgroundColor = "black"
+        volumen1.style.backgroundColor = "black"
+        video.volume = 0.4
+        if(volumen4.style.backgroundColor == "black" && 
+        volumen5.style.backgroundColor == "black"){
+        volumen4.style.backgroundColor = "grey"
+        volumen5.style.backgroundColor = "grey"
+        }
+    }else{
+    volumen3.style.backgroundColor = "black"
+    volumen2.style.backgroundColor = "black"
+    volumen1.style.backgroundColor = "black"
+    volumen4.style.backgroundColor = "grey"
+    volumen5.style.backgroundColor = "grey"
+    }
+})
+
+volumen4.addEventListener("click",function(){
+    if(volumen4.style.backgroundColor != "black"){
+        volumen4.style.backgroundColor = "black"
+        volumen3.style.backgroundColor = "black"
+        volumen2.style.backgroundColor = "black"
+        volumen1.style.backgroundColor = "black"
+        video.volume = 0.5
+        if(volumen5.style.backgroundColor == "black"&&
+        volumen4.style.backgroundColor == "black"){
+        volumen5.style.backgroundColor = "grey"
+        volumen4.style.backgroundColor = "grey"
+        volumen3.style.backgroundColor = "black"
+        volumen2.style.backgroundColor = "black"
+        volumen1.style.backgroundColor = "black"    
+        }
+    }else{
+    volumen5.style.backgroundColor = "grey"
+    volumen4.style.backgroundColor = "black"
+    volumen3.style.backgroundColor = "black"
+    volumen2.style.backgroundColor = "black"
+    volumen1.style.backgroundColor = "black"
+    }
+})
+
+volumen5.addEventListener("click",function(){
+    if(volumen5.style.backgroundColor != "black"){
+        volumen5.style.backgroundColor = "black"
+        video.volume = 0.9
+        volumen1.style.backgroundColor = "black"
+        volumen2.style.backgroundColor = "black"
+        volumen3.style.backgroundColor = "black"
+        volumen4.style.backgroundColor = "black"
+        }
 })
 
 botonBigSmall.addEventListener("click",function(){
